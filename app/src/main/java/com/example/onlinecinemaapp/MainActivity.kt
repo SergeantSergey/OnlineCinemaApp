e1fb28c930bcc14273619f5f5f7c2b3bd11a402f
sergey.startsev@x5.ru <56startcev5611RUS>
2021-10-27 20:53:31 +0300

    Add implementation

--- app/src/main/java/com/example/onlinecinemaapp/MainActivity.kt
+++ app/src/main/java/com/example/onlinecinemaapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.onlinecinemaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.onlinecinemaapp.feature.moviesList.ui.MoviesListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setFragment(MoviesListFragment())
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(android.R.id.content, fragment).commit()
     }
 }
---
+++ app/src/main/java/com/example/onlinecinemaapp/feature/moviesList/ui/MoviesListFragment.kt
@@ -0,0 +1,7 @@
+package com.example.onlinecinemaapp.feature.moviesList.ui
+
+import androidx.fragment.app.Fragment
+import com.example.onlinecinemaapp.R
+
+class MoviesListFragment:Fragment(R.layout.fragment_movies_list) {
+}
